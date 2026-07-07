930e59f81bcecb7fd39dabca34ab8d3fc591b6f6
Fantasy Maniac <muhieti@gmail.com>
2026-07-07 11:31:34 +0330

    no change

--- Magic Mail Mayhem/Assets/Assets/Scripts/NetworkInputData.cs
+++ Magic Mail Mayhem/Assets/Assets/Scripts/NetworkInputData.cs
@@ -7,24 +7,3 @@ public struct NetworkInputData : INetworkInput
     public Vector2 LookDelta;
     public NetworkBool Sprint;
 }
-
-
-
-// using Fusion;
-// using UnityEngine;
-
-// public struct NetworkInputData : INetworkInput
-// {
-//     // public Vector3 Direction;
-
-//     // WASD movement
-//     public Vector2 Move;
-
-//     // Absolute look angles:
-//     // x = yaw
-//     // y = pitch
-//     public Vector2 LookRotation;
-
-//     // Sprint button
-//     public NetworkBool Sprint;
-// }
